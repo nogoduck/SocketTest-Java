080dd5d1421675c813b5d09abad190011514c455
adu <nogo22@naver.com>
2021-11-12 03:02:27 +0900

    1:n 통신을 위해 클라이언트 작성

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,7 +1,62 @@
 package application;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class Client {
+    Socket socket;
 
+    public Client(Socket socket){
+        this.socket = socket;
+        receive();
+    }
+
+    //클라이언트로부터 메시지를 전달 받는 메소드
+    public void receive(){
+
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //클라이언트로부터 내용을 반복적으로 받기 위함
+                    while(true){
+                        InputStream in = socket.getInputStream();
+                        //한번에 512바이트씩 받는다.
+                        byte[] buffer = new byte[512];
+                        int length = in.read(buffer);
+                        //메세지를 읽다가 오류 처리
+                        while(length == -1) throw new IOException();
+                        System.out.println("[메시지 수신 성공] " + socket.getRemoteSocketAddress() + ": "
+                        + Thread.currentThread().getName());
+                        //UTF-8로 한글도 처리가능하게 세팅
+                        String message =  new String(buffer, 0, length, "UTF-8");
+
+                        //1:n의 통신을 위해 다른 클라이언트에도 정보를 전송해 주는 반복문
+                        for(Client client: Main.clients){
+                            client.send(message);
+                        }
+
+                    }
+                } catch(Exception e){
+                        //예외를 구체적으로 처리하기 위해 중첩해서 사용
+                    try{
+                        System.out.println("[메세지 수신 오류] "
+                                + socket.getRemoteSocketAddress()
+                                + ": " + Thread.currentThread().getName());
+                    } catch (Exception e2){
+                        e2.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        //threadPool에 스레드를 등록해서 안정적으로 관리하게 해준다.
+        Main.threadPool.submit(thread);
+    }
+
+    //클라이언트로부터 메시지를 전송하는 메소드
+    public void send(String message){
+
+    }
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -6,21 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.ServerSocket;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("MainUI.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
+	// 여러개의 스레드를 효율적으로 관리하기 위한 라이브러리
+	// threadPool 사용하면 기본적인 스레드 숫자에 제한을 두게 되어 갑장스러운 트래픽에 대응할 수 있다.
+	public static ExecutorService threadPool;
+	public static Vector<Client> clients = new Vector<Client>();
+
+	ServerSocket serverSocket;
+
+	// 서버를 구동시켜서 클라이언트의 연결을 기다리는 메소드
+	public void startServer(String IP, int port){
+		
 	}
 
+	// 서버의 작동을 중지하는 메소드
+	public void stopServer(){
 
+	}
+	
+	// UI를 생성하고, 프로그램을 동작시키는 메소드
+	@Override
+	public void start(Stage primaryStage){
+		
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
